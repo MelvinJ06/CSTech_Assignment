47e8878661409d9958e8c1f3a43e52ac9a6c07af
MelvinJ06 <melvinjames0602@gmail.com>
2025-10-16 13:35:41 +0530

    first commit

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ app.use("/uploads", express.static("uploads"));
 app.use("/api/auth", authRoutes);
 app.use("/api/agents", agentRoutes);
 app.use("/api/upload", uploadRoutes);
+app.get("/api/auth/test", (req, res) => {
+  res.json({ message: "Backend is working!" });
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
